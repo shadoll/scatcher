f37b33be9a2b6410749d6ab350ac81b8a7547755
sHa <sha@shadoll.dev>
2024-03-12 16:38:32 +0200

    Refactor request history storage and add Docker Compose file

--- app.py
+++ app.py
@@ -4,22 +4,25 @@ import json
 
 app = FastAPI()
 
-LIMIT = 10
+HISTORY_LIMIT = 10
+HISTORY_FILE = "storage/requests.json"
 
-def store_last_request(last_request, filename='requests.json'):
+
+def store_last_request(last_request, filename=HISTORY_FILE):
     try:
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             data = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         data = []
 
     data.append(last_request)
 
-    if len(data) > LIMIT:
+    if len(data) > HISTORY_LIMIT:
         data.pop(0)
 
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         json.dump(data, f)
+
 
 @app.post("/", status_code=status.HTTP_200_OK)
 async def webhook_handler(request: Request, response: Response):
@@ -35,25 +38,26 @@ async def webhook_handler(request: Request, response: Response):
     }
     store_last_request(last_request)
 
-
     response.status_code = status.HTTP_200_OK
 
     return {"status": "ok"}
 
+
 @app.get("/__last_request__", status_code=status.HTTP_200_OK)
 async def last_requests():
     try:
-        with open('requests.json', 'r') as f:
+        with open(HISTORY_FILE, "r") as f:
             data = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         data = []
 
     return data[-1:]
 
+
 @app.get("/__history__", status_code=status.HTTP_200_OK)
 async def history():
     try:
-        with open('requests.json', 'r') as f:
+        with open(HISTORY_FILE, "r") as f:
             data = json.load(f)
     except (FileNotFoundError, json.JSONDecodeError):
         data = []
